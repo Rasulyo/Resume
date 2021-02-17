ea62c16c8677c9d7df1b8feaac135a9e9079a004
unknown <kkg.rasul@gmail.com>
2021-02-17 15:37:40 +0600

    Commit

---
+++ js/resume.js
@@ -0,0 +1,81 @@
+VANTA.NET({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xe1d0d0
+  })
+
+//   VANTA.FOG({
+//     el: "#header",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     highlightColor: 0x7945fa,
+//     midtoneColor: 0x9d8582
+//   })
+
+// VANTA.NET({
+//     el: "#header",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     color: 0xe1d0d0
+//   })
+
+  VANTA.WAVES({
+    el: "#projects",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3d6484
+  })
+//   VANTA.WAVES({
+//     el: "#header",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     color: 0x3d6484
+//   })
+
+  VANTA.GLOBE({
+    el: "#contacts",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x18020a
+  })
+
+  VANTA.BIRDS({
+    el: "#about",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xd0dae6
+  })
